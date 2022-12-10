2d23430f531fc3359b6383777b47695a98941e8f
longhaoyang <longhaoyang@ee547work>
2022-12-09 18:27:24 -0800

    Finish post styling

--- public/js/common.js
+++ public/js/common.js
@@ -2,9 +2,11 @@ $("#postTextarea").keyup(event => {
     var textbox = $(event.target);
     var value = textbox.val().trim();
 
-    var isModal = textbox.parents(".modal").length == 1;
+    // var isModal = textbox.parents(".modal").length == 1;
     
-    var submitButton = isModal ? $("#submitReplyButton") : $("#submitPostButton");
+    // var submitButton = isModal ? $("#submitReplyButton") : $("#submitPostButton");
+
+    var submitButton = $("#submitPostButton");
 
     if(submitButton.length == 0) return alert("No submit button found");
 
@@ -38,22 +40,83 @@ function createPostHtml(postData) {
 
     var postedBy = postData.postedBy; 
 
+    if(postedBy._id === undefined) {
+        return console.log("User object not populated");
+    }
+    var displayName = postedBy.firstName + " " + postedBy.lastName;
+    var timestamp = timeDifference(new Date(), new Date(postData.createdAt));
 
+    var buttons = "";
+    return `<div class='post'>
 
-    return `<div class='post>
                 <div class='mainContentContainer'>
                     <div class='userImageContainer'>
                         <img src='${postedBy.profilePic}'>
                     </div>
                     <div class='postContentContainer'>
                         <div class='header'>
+                            <a href='/profile/${postedBy.username}' class='displayName'>${displayName}</a>
+                            <span class='username'>@${postedBy.username}</span>
+                            <span class='date'>${timestamp}</span>
                         </div>
                         <div class='postBody'>
                             <span>${postData.content}</span>
                         </div>
                         <div class='postFooter'>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class="far fa-comments"></i>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer'>
+                                <button class='retweetButton'>
+                                    <i class="fa-solid fa-share"></i>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer'>
+                                <button class='likeButton'>
+                                    <i class="fa-sharp fa-solid fa-heart"></i>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>`;
 };
+
+function timeDifference(current, previous) {
+
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+        if(elapsed/1000 < 30) return "Just now";
+        
+        return Math.round(elapsed/1000) + ' seconds ago';   
+    }
+
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    }
+
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
+    }
+
+    else if (elapsed < msPerMonth) {
+        return Math.round(elapsed/msPerDay) + ' days ago';   
+    }
+
+    else if (elapsed < msPerYear) {
+        return Math.round(elapsed/msPerMonth) + ' months ago';   
+    }
+
+    else {
+        return Math.round(elapsed/msPerYear ) + ' years ago';   
+    }
+}
